a13838a7874dcb9381d8c6e5b20e27e9ec6b3d31
Kevin Cearns <kevin@entuit.com>
2021-11-15 20:52:40 +0000

    commit to close

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import UploadFile from '../components/UploadFile'
+import Header from '../components/Header'
+import FileList from '../components/FileList'
+import { ethers } from 'ethers'
+import { Dialog } from '@headlessui/react'
 
 const Index = () => {
+    const [currentAccount, setCurrentAccount] = useState(null)
+    const [isOpen, setIsOpen] = useState(true)
+
+    const closeModal = () => {
+        setIsOpen(false)
+    }
+
+    const openModal = () => {
+        setIsOpen(true)
+    }
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { ethereum } = window
+            if (!ethereum) {
+                console.log('Make sure you have Metamask')
+                return
+            } else {
+                console.log('We have the ethereum object', ethereum)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window
+            if (!ethereum) {
+                alert('Get metamask')
+                return
+            }
+
+            const accounts = await ethereum.request({
+                method: 'eth_requestAccounts'
+            })
+
+            console.log('Connected:', accounts[0] )
+            setCurrentAccount(accounts[0])
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        checkIfWalletIsConnected()
+    }, [])
+    
+
     return (
+        <>
+        <Header currentAccount={currentAccount} />
         <div className="flex h-screen justify-center items-center">
             <div>
-                <UploadFile />
+                {
+                    currentAccount && (
+                        <>
+                        <UploadFile />
+                        <FileList />
+                        </>
+                    )
+                }
             </div>
+            {
+                !currentAccount && (
+                    <button
+                    className="bg-blue-400 rounded-lg text-white p-4 font-bold"
+                    onClick={connectWallet}
+                >
+                    Connect Your Wallet
+                </button>
+                )
+            }
         </div>
+        </>
     )
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const navigation = [
+    // {
+    //     name: 'Dashboard',
+    //     href: '#',
+    //     current: true
+    // },
+]
+
+const navImage = 'https://ipfs.infura.io/ipfs/QmT21p5219bzmQxfzVNyJNprZAp1kJp3VyyAzUYoooLXMX'
+
+export { navigation, navImage }
